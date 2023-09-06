5e3455210db7e0a8723de70522f3ecf201080bc1
Nate Shoffner <nate.shoffner@gmail.com>
2023-09-06 05:07:32 -0400

    Collection category stats

--- src/tenji/model/user/collection.py
+++ src/tenji/model/user/collection.py
@@ -4,7 +4,12 @@ from pydantic import BaseModel
 from tenji.model.item.item import Item
 from tenji.model.paginated import Pagination
 
+class CollectionStats(BaseModel):
+    owned: int = 0
+    ordered: int = 0
+    wished: int = 0
 
 class Collection(BaseModel):
     items: list[Item] = []
+    stats: Optional[CollectionStats] = None
     pagination: Optional[Pagination] = None
--- src/tenji/parser/user/collection.py
+++ src/tenji/parser/user/collection.py
@@ -1,5 +1,5 @@
-from tenji.model.category import ItemCategory, get_item_category_from_str
-from tenji.model.user.collection import Collection
+from tenji.model.category import ItemCategory
+from tenji.model.user.collection import Collection, CollectionStats
 from tenji.model.item.item import Item
 from tenji.parser.parser_base import ParserBase
 
@@ -7,6 +7,12 @@ from tenji.parser.parser_base import ParserBase
 class CollectionParser(ParserBase):
     def parse(self) -> Collection:
         items = []
+
+        cat_links = self._soup.select_one("div.results-toggles").select("a")
+        owned = self.try_extract_number(cat_links[0].text)
+        ordered = self.try_extract_number(cat_links[1].text)
+        wished = self.try_extract_number(cat_links[2].text)
+        stats = CollectionStats(owned=owned, ordered=ordered, wished=wished)
 
         results = self._soup.select("div.results > div.result")
 
@@ -32,4 +38,4 @@ class CollectionParser(ParserBase):
             items.append(item)
 
         pagination = self.try_parse_pagination()
-        return Collection(items=items, pagination=pagination)
+        return Collection(items=items, stats=stats, pagination=pagination)
